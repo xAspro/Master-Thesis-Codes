355e61746c17240016ae1a42ef3f28614bb7c808
xAspro <sooryabalan.m@tifr.res.in>
2024-11-06 11:47:51 +0530

    Updated CHECK.py

--- CHECK.py
+++ CHECK.py
@@ -36,72 +36,6 @@ print("phi is ", phi(1, Vbin(dVdz, dz, dm)) )
 
 
 
-
-
-
-
-
-
-from scipy.integrate import quad
-
-# Integrate over z = 5.15 ± 0.5 or z = 5.24 ± 0.5
-def differential_volume(z, cosmo=cosmo):
-    omega = (A / 41253.0) * 4.0 * np.pi
-    return cd.diff_comoving_volume(z, **cosmo) * omega
-
-# Numerical integration for Vbin over the redshift range
-z_min = 5.15 - 0.5
-z_max = 5.15 + 0.5
-Vbin_z1, _ = quad(differential_volume, z_min, z_max)
-
-z_min = 5.24 - 0.5
-z_max = 5.24 + 0.5
-Vbin_z2, _ = quad(differential_volume, z_min, z_max)
-
-print()
-
-print("phi is ", phi(1, Vbin_z1) )
-print("phi is ", phi(1, Vbin_z2) )
-
-
-print()
-
-
-
-
-
-
-
-
-
-
-############################################################################################################
-############################################################################################################
-
-
-from scipy.integrate import quad
-
-def differential_volume(z, area, cosmo=cosmo):
-    omega = (area / 41253.0) * 4.0 * np.pi
-    return cd.diff_comoving_volume(z, **cosmo) * omega
-
-# Integrate over the redshift ranges for both papers
-zrange_onoue = (5.15 - 0.5, 5.15 + 0.5)
-Vbin_onoue, _ = quad(differential_volume, zrange_onoue[0], zrange_onoue[1], args=(A, cosmo))
-
-zrange_kocevski = (5.24 - 0.5, 5.24 + 0.5)
-Vbin_kocevski, _ = quad(differential_volume, zrange_kocevski[0], zrange_kocevski[1], args=(A, cosmo))
-
-print("phi for Onoue:", phi(1, Vbin_onoue))
-print("phi for Kocevski:", phi(1, Vbin_kocevski))
-
-
-
-
-
-
-
-
 # From https://www.astro.ucla.edu/~wright/CosmoCalc.html
 # H0 = 70
 # OmegaM = 0.3
@@ -117,12 +51,3 @@ print("phi for Kocevski:", phi(1, Vbin_kocevski))
 # comoving radius = 7851.1 Mpc
 # r^2 = 61639771.21
 # comoving volume = 2027.029 Gpc3
-
-
-# # From cosmolopy.distance
-
-# z = 5.24
-# comoving volume per steradian = 31147524593.58081
-
-# z = 5.15
-# comoving volume per steradian = 31462036093.976078
