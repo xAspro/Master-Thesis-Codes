4615492061e0ce9fd14a4610a3f6cb4e00a70123
xAspro <sooryabalan.m@tifr.res.in>
2024-11-22 01:47:08 +0530

    Worked it out for +/-0.05 range of what Kocevski and Onoue has qouted. Now have to figure out if this is what they chose for their dm?

--- CHECK.py
+++ CHECK.py
@@ -64,54 +64,136 @@ plt.plot(dm_values, np.full_like(dm_values, given_values[4]), 'g-', label='Main 
 plt.fill_between(dm_values, given_values[3], given_values[5], color='green', alpha=0.3)
 
 
-# Manually check for the range where phi crosses the given values
-range_5_15 = (phi_values[:, 0] > 1.03 * 10**-5)
-range_5_24 = (phi_values[:, 1] > 1.07 * 10**-5)
 
-index_5_15 = np.where(range_5_15)[0][-1]
-index_5_24 = np.where(range_5_24)[0][-1]
+def range(phi, value):
+    return (phi > value)
 
-print("range_5_15 is ", range_5_15)
-print("range_5_24 is ", range_5_24)
+def index(phi, value):
+    return np.where(range(phi, value))[0][-1]
 
-print("index_5_15 is ", index_5_15)
-print("index_5_24 is ", index_5_24)
+print("range is ", range(phi_values[:, 0], 1.03 * 10**-5))
+print("index is ", index(phi_values[:, 0], 1.03 * 10**-5))
+print("range is ", range(phi_values[:, 0], 1.025 * 10**-5))
+print("index is ", index(phi_values[:, 0], 1.025 * 10**-5))
+print("range is ", range(phi_values[:, 0], 1.035 * 10**-5))
+print("index is ", index(phi_values[:, 0], 1.035 * 10**-5))
+
+print("range is ", range(phi_values[:, 1], 1.07 * 10**-5))
+print("index is ", index(phi_values[:, 1], 1.07 * 10**-5))
+print("range is ", range(phi_values[:, 1], 1.065 * 10**-5))
+print("index is ", index(phi_values[:, 1], 1.065 * 10**-5))
+print("range is ", range(phi_values[:, 1], 1.075 * 10**-5))
+print("index is ", index(phi_values[:, 1], 1.075 * 10**-5))
+
+index_5_15_1_03 = index(phi_values[:, 0], 1.03 * 10**-5)
+index_5_15_1_025 = index(phi_values[:, 0], 1.025 * 10**-5)
+index_5_15_1_035 = index(phi_values[:, 0], 1.035 * 10**-5)
+
+index_5_24_1_07 = index(phi_values[:, 1], 1.07 * 10**-5)
+index_5_24_1_065 = index(phi_values[:, 1], 1.065 * 10**-5)
+index_5_24_1_075 = index(phi_values[:, 1], 1.075 * 10**-5)
+
+# # Manually check for the range where phi crosses the given values
+# range_5_15 = (phi_values[:, 0] > 1.03 * 10**-5)
+# range_5_24 = (phi_values[:, 1] > 1.07 * 10**-5)
+
+# index_5_15 = np.where(range_5_15)[0][-1]
+# index_5_24 = np.where(range_5_24)[0][-1]
+
+# print("range_5_15 is ", range_5_15)
+# print("range_5_24 is ", range_5_24)
+
+# print("index_5_15 is ", index_5_15)
+# print("index_5_24 is ", index_5_24)
 
 # Fit a straight line between the two values and interpolate
-def linear_interpolation(x, y, target):
+def linear_interpolation(y, x, target):
     print("x is ", x)
     print("y is ", y)
     print("target is ", target)
-    return np.interp(target, y, x)
+    res = y[0] + (target - x[0]) * (y[1] - y[0]) / (x[1] - x[0])
+    return res
 
-print("dm_values[index_5_15] is ", dm_values[index_5_15])
-print("dm_values[index_5_15+1] is ", dm_values[index_5_15+1])
-print("phi_values[index_5_15, 0] is ", phi_values[index_5_15, 0])
-print("phi_values[index_5_15+1, 0] is ", phi_values[index_5_15+1, 0])
+# print("dm_values[index_5_15] is ", dm_values[index_5_15])
+# print("dm_values[index_5_15 + 1] is ", dm_values[index_5_15 + 1])
+# print("phi_values[index_5_15, 0] is ", phi_values[index_5_15, 0])
+# print("phi_values[index_5_15 + 1, 0] is ", phi_values[index_5_15 + 1, 0])
 
-print("dm_values[index_5_24] is ", dm_values[index_5_24])
-print("dm_values[index_5_24+1] is ", dm_values[index_5_24+1])
-print("phi_values[index_5_24, 1] is ", phi_values[index_5_24, 1])
-print("phi_values[index_5_24+1, 1] is ", phi_values[index_5_24+1, 1])
+# print("dm_values[index_5_24] is ", dm_values[index_5_24])
+# print("dm_values[index_5_24 + 1] is ", dm_values[index_5_24 + 1])
+# print("phi_values[index_5_24, 1] is ", phi_values[index_5_24, 1])
+# print("phi_values[index_5_24 + 1, 1] is ", phi_values[index_5_24 + 1, 1])
 
-intersection_5_15 = linear_interpolation(
-    [dm_values[index_5_15], dm_values[index_5_15+1]], 
-    [phi_values[index_5_15, 0], phi_values[index_5_15+1, 0]], 
+intersection_5_15_1_025 = linear_interpolation(
+    [dm_values[index_5_15_1_025], dm_values[index_5_15_1_025 + 1]], 
+    [phi_values[index_5_15_1_025, 0], phi_values[index_5_15_1_025 + 1, 0]], 
+    1.025 * 10**-5
+)
+intersection_5_15_1_03 = linear_interpolation(
+    [dm_values[index_5_15_1_03], dm_values[index_5_15_1_03 + 1]],
+    [phi_values[index_5_15_1_03, 0], phi_values[index_5_15_1_03 + 1, 0]],
     1.03 * 10**-5
 )
-intersection_5_24 = linear_interpolation(
-    [dm_values[index_5_24], dm_values[index_5_24+1]], 
-    [phi_values[index_5_24, 1], phi_values[index_5_24+1, 1]], 
+intersection_5_15_1_035 = linear_interpolation(
+    [dm_values[index_5_15_1_035], dm_values[index_5_15_1_035 + 1]],
+    [phi_values[index_5_15_1_035, 0], phi_values[index_5_15_1_035 + 1, 0]],
+    1.035 * 10**-5
+)
+
+intersection_5_24_1_065 = linear_interpolation(
+    [dm_values[index_5_24_1_065], dm_values[index_5_24_1_065 + 1]],
+    [phi_values[index_5_24_1_065, 1], phi_values[index_5_24_1_065 + 1, 1]],
+    1.065 * 10**-5
+)
+intersection_5_24_1_07 = linear_interpolation(
+    [dm_values[index_5_24_1_07], dm_values[index_5_24_1_07 + 1]],
+    [phi_values[index_5_24_1_07, 1], phi_values[index_5_24_1_07 + 1, 1]],
     1.07 * 10**-5
+)
+intersection_5_24_1_075 = linear_interpolation(
+    [dm_values[index_5_24_1_075], dm_values[index_5_24_1_075 + 1]],
+    [phi_values[index_5_24_1_075, 1], phi_values[index_5_24_1_075 + 1, 1]],
+    1.075 * 10**-5
 )
 
 print()
-print("Intersection for z = 5.15 is ", intersection_5_15)
-print("Intersection for z = 5.24 is ", intersection_5_24)
+print("intersection_5_15_1_025 is ", intersection_5_15_1_025)
+# print("dm_values[index_5_15_1_025] is ", dm_values[index_5_15_1_025])
+# print("dm_values[index_5_15_1_025 + 1] is ", dm_values[index_5_15_1_025 + 1])
+# print("phi_values[index_5_15_1_025, 0] is ", phi_values[index_5_15_1_025, 0])
+# print("phi_values[index_5_15_1_025 + 1, 0] is ", phi_values[index_5_15_1_025 + 1, 0])
+print("intersection_5_15_1_03 is ", intersection_5_15_1_03)
+# print("dm_values[index_5_15_1_03] is ", dm_values[index_5_15_1_03])
+# print("dm_values[index_5_15_1_03 + 1] is ", dm_values[index_5_15_1_03 + 1])
+# print("phi_values[index_5_15_1_03, 0] is ", phi_values[index_5_15_1_03, 0])
+# print("phi_values[index_5_15_1_03 + 1, 0] is ", phi_values[index_5_15_1_03 + 1, 0])
+print("intersection_5_15_1_035 is ", intersection_5_15_1_035)
+# print("dm_values[index_5_15_1_035] is ", dm_values[index_5_15_1_035])
+# print("dm_values[index_5_15_1_035 + 1] is ", dm_values[index_5_15_1_035 + 1])
+# print("phi_values[index_5_15_1_035, 0] is ", phi_values[index_5_15_1_035, 0])
+# print("phi_values[index_5_15_1_035 + 1, 0] is ", phi_values[index_5_15_1_035 + 1, 0])
+print()
+print("intersection_5_24_1_065 is ", intersection_5_24_1_065)
+# print("dm_values[index_5_24_1_065] is ", dm_values[index_5_24_1_065])
+# print("dm_values[index_5_24_1_065 + 1] is ", dm_values[index_5_24_1_065 + 1])
+# print("phi_values[index_5_24_1_065, 1] is ", phi_values[index_5_24_1_065, 1])
+# print("phi_values[index_5_24_1_065 + 1, 1] is ", phi_values[index_5_24_1_065 + 1, 1])
+print("intersection_5_24_1_07 is ", intersection_5_24_1_07)
+# print("dm_values[index_5_24_1_07] is ", dm_values[index_5_24_1_07])
+# print("dm_values[index_5_24_1_07 + 1] is ", dm_values[index_5_24_1_07 + 1])
+# print("phi_values[index_5_24_1_07, 1] is ", phi_values[index_5_24_1_07, 1])
+# print("phi_values[index_5_24_1_07 + 1, 1] is ", phi_values[index_5_24_1_07 + 1, 1])
+print("intersection_5_24_1_075 is ", intersection_5_24_1_075)
+# print("dm_values[index_5_24_1_075] is ", dm_values[index_5_24_1_075])
+# print("dm_values[index_5_24_1_075 + 1] is ", dm_values[index_5_24_1_075 + 1])
+# print("phi_values[index_5_24_1_075, 1] is ", phi_values[index_5_24_1_075, 1])
+# print("phi_values[index_5_24_1_075 + 1, 1] is ", phi_values[index_5_24_1_075 + 1, 1])
+print()
 
 # Plot the intersection points
-plt.plot(intersection_5_15, 1.03 * 10**-5, 'ro', label='Intersection for z = 5.15')
-plt.plot(intersection_5_24, 1.07 * 10**-5, 'go', label='Intersection for z = 5.24')
+plt.plot([intersection_5_15_1_025, intersection_5_15_1_03, intersection_5_15_1_035], [1.025 * 10**-5, 1.03 * 10**-5, 1.035 * 10**-5], 'ro')
+plt.plot([intersection_5_24_1_065, intersection_5_24_1_07, intersection_5_24_1_075], [1.065 * 10**-5, 1.07 * 10**-5, 1.075 * 10**-5], 'go')
+
 
 # Add circles around the intersection points
 # plt.gca().add_patch(plt.Circle((intersection_5_15, 1.03 * 10**-5), 0.5e-6, color='red', fill=False))
@@ -267,7 +349,7 @@ plt.show()
 #     elif special == 'croom_comparison':
 #         # These M1450 bins result in the Mgz2 bins of Croom09.  The
 #         # 1.23 converts between the two magnitudes (Eqn B8 of Ross13).
-#         bins = np.arange(-30,-19.5,0.5)+1.23
+#         bins = np.arange(-30,-19.5,0.5) + 1.23
 #     elif special == 'croom_comparison_Mgz2':
 #         # These Mgz2 bins of Croom09.  
 #         bins = np.arange(-30,-19.5,0.5)
